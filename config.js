86237757a2b4015948e5ffae4f7fa94c6bbc8302
dityaastawa <90491847+Dityaastawa@users.noreply.github.com>
2022-06-07 15:48:24 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 
-global.owner = ['6281220670449','0']
-global.packname = 'Bot Whatsapp'
-global.author = 'TRASH'
+global.owner = ['6285237596750','0']
+global.packname = 'Ditya Botz'
+global.author = 'Ditya'
 global.sessionName = 'data'
 global.prefa = ['','!','.','#']
-global.ownername = 'KurrXd'
+global.ownername = 'Dityaastawa'
 global.sp = '>'
 global.mess = {
     admin: 'Fitur Khusus Admin Group!',
